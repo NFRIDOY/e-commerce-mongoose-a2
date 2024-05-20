ccc2dabdf8bb32b0d646d319cd28cc2ffb7b5d96
NF RIDOY <nfridoy@gmail.com>
2024-05-20 21:48:26 +0600

    interface, qus

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,10 @@ const app = express();
 //parsers
 app.use(express.json());
 
-// app.use("/api/movies", MovieRoutes);
+// app.use("/api/movies", ProductRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Next!");
+  res.send("E-Commerce Mongoose Server is Running");
 });
 
 export default app;
